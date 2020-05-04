c1cbdee288c9f4add7e2a1bb08fc7d1b340cf6fb
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-05-04 23:01:29 +0100

    add read more buttons to news cards

--- client/src/NewsCard/NewsCard.js
+++ client/src/NewsCard/NewsCard.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewsCard.css';
 
 function NewsCard({ story }) {
+
+const [more, setMore] = useState(false);
+
+const clickHandler = () => {
+  console.log('was clik')
+  setMore((state) => !state);
+}
+
   return (
     <div className="card">
       <div className="card_content-wrapper">
       <div className="card_source">{story.source.name}</div>
        <div className="card_headline">{story.title}</div>
-       <div className="card_standfirst">{story.description &&  story.description.split('.', 2).join('')}</div>
-       <div className="card_btn">READ MORE</div>
+       {!more ? <div className="card_standfirst">{story.description &&  story.description.split('.', 2).join('')}</div>
+       : <div>{story.content.split('[')[0]}</div> }
+       {!more && story.content
+       ?<div className='card_btn' onClick={clickHandler}>READ MORE </div>
+       :<a className='card_btn' href={story.url} target='_blank'>FULL STORY </a>
+       }
 
        </div>
     </div>
