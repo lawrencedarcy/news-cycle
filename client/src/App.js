361911d538657f32dc86d694942f75f9f890bf19
lawrencedarcy <lawrencedwakefield@gmail.com>
2020-06-15 15:40:02 +0100

    git submodule add <url> cors-proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,46 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import NavBar from './NavBar/NavBar';
 import Dashboard from './Dashboard/Dashboard';
 import axios from 'axios';
 
-
 function App() {
-
   const [topStories, setTopStories] = useState([]);
 
-  const sectionChange = (section) => {
-
-    axios.get(`https://newsapi.org/v2/everything?q=uk ${section}&apiKey=0dc27b8b03d146529a79da477d2aaa48`
-    ).then(res => {
-      const stories = res.data.articles;
-      setTopStories(() => stories);
-    });
-
-  } 
+  const sectionChange = section => {
+    axios
+      .get(
+        `https://newsapi.org:443/v2/everything?q=uk${section}&apiKey=0dc27b8b03d146529a79da477d2aaa48`
+      )
+      .then(res => {
+        const stories = res.data.articles;
+        console.log(res);
+        setTopStories(() => stories);
+      });
+  };
 
   useEffect(() => {
     const TOP_NEWS_URL =
-      'https://newsapi.org/v2/top-headlines?' +
+      'https://' +
+      'newsapi.org/v2/top-headlines?' +
       'country=gb&' +
       'apiKey=0dc27b8b03d146529a79da477d2aaa48';
 
-  
-    axios.get(TOP_NEWS_URL).then(res => {
-      const stories = res.data.articles;
-      setTopStories(() => stories);
-    });
-
+    axios
+      .get('https://news-app-proxy.herokuapp.com/',
+        {headers: { 'Target-Endpoint': TOP_NEWS_URL }}
+      )
+      .then(res => {
+        console.log(res);
+        const stories = res.data.articles;
+        setTopStories(() => stories);
+      });
   }, []);
 
- 
   return (
-    <div >
-      <NavBar sectionChange={sectionChange}/>
-      <Dashboard topStories={topStories}/>
+    <div>
+      <NavBar sectionChange={sectionChange} />
+      <Dashboard topStories={topStories} />
     </div>
   );
 }
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('proxy-middleware');
 module.exports = app => {
   // setup proxies
   // 1st api
-  app.use("https://newsapi.org/v2/top-headlines?country=gb&apiKey=0dc27b8b03d146529a79da477d2aaa48", proxy("https://api.newscycle.io/api"));
+  app.use("/newscycle/topheadlines", proxy("https://newsapi.org/v2/top-headlines?country=gb&apiKey=0dc27b8b03d146529a79da477d2aaa48"));
  
 
   
